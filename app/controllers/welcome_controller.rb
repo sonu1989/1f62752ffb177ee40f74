e33a5720ac031e28436537fd85c9c144e505747e
Sonu Singh <sonus.ror@gmail.com>
2020-09-17 17:25:55 +0530

    Added pagination with styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,9 +4,9 @@ class WelcomeController < ApplicationController
     if params[:search_term].present?
       search_keyword = "%#{params[:search_term].downcase}%"
       # Here I am not using ILIKE becuase app is using sqlite and Ilike is not supported by it.
-      @tv_shows = TvShow.includes(:channel, :favorite_by_users).where('lower(channels.name) LIKE (?) OR lower(tv_shows.name) LIKE (?)', search_keyword, search_keyword).references(:channels)
+      @tv_shows = TvShow.includes(:channel, :favorite_by_users).where('lower(channels.name) LIKE (?) OR lower(tv_shows.name) LIKE (?)', search_keyword, search_keyword).references(:channels).paginate(page: params[:page], per_page: 10)
     else
-      @tv_shows = TvShow.includes(:channel, :favorite_by_users)
+      @tv_shows = TvShow.includes(:channel, :favorite_by_users).paginate(page: params[:page], per_page: 10)
     end
   end
 end
